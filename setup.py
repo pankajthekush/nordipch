7aee39f232fdef91caa6769482f8727b0bebc777
pankajthekush <pankajthekush@gmail.com>
2020-01-06 12:08:55 +0530

    Fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nordvpch",
+    name="nordipch",
     version="0.0.1",
     author="Pankaj Kumar",
     author_email="pankajthekush@gmail.com",
