d5049bd1553b73ffc7e067c89937cb3275680584
pankajthekush <pankajthekush@gmail.com>
2020-01-07 15:33:25 +0530

    Updated Logging
    Added logger file to keep the logs

--- nordipch/nordipch.py
+++ nordipch/nordipch.py
@@ -4,7 +4,11 @@ import sys
 from time import sleep
 import ipaddress
 import subprocess
+import logging
 
+
+logging.basicConfig(filename='nordipch.log', level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s:%(lineno)d')
 
 nord_api = "https://api.nordvpn.com/server"
 current_ip_api = "http://myip.dnsomatic.com"
@@ -19,10 +23,13 @@ def return_nord_json():
             nord_api_text = requests.get(nord_api).text
         except Exception as error:
             sleep(3)
-            print(f"Server Not Found.Retrying {retry_count}...")
+            logging.debug(f"{error}, Retrying : {retry_count}...")
             retry_count += 1
             if retry_count >= max_retry_count:
+                logging.debug(error)
+                logging.debug("Nord API JSON request : Fail")
                 return error
+    logging.debug("Nord API JSON request : Success")
     return nord_api_text
     
 def  get_current_ip():
@@ -36,9 +43,10 @@ def  get_current_ip():
             ipaddress.ip_address(current_api_text)  #This validates the IP returned
         except Exception as error:
             sleep(3)
-            print(f"Current IP  Not Determined.Retrying {retry_count}...")
+            logging.debug(f"Error {error}.Retrying {retry_count}...")
             retry_count += 1
             if retry_count >= max_retry_count:
+                logging.debug(error)
                 return error
     
     last_digit=  int(current_api_text[-1:])
@@ -46,15 +54,15 @@ def  get_current_ip():
         last_tow_digit = int(current_api_text[-2:])
         last_digit = str(int(last_tow_digit)-1)
         v_ip = current_api_text[:-2] + last_digit
+        logging.debug(f"Current IP : {v_ip}")
         return v_ip
 
     else:
         last_digit = str(int(last_digit-1))
         v_ip = current_api_text[:-1] + last_digit
+        logging.debug(f"Current IP : {v_ip}")
         return v_ip
 
-
-    
 
 
 def status():
@@ -68,28 +76,41 @@ def status():
         ip_address = ip_data['ip_address'].strip()
         if v_ip == ip_address:
             id = ip_data['id']
+            logging.debug(f"CONNECTED, {v_ip},{id}")
             return "CONNECTED", v_ip,id
+    logging.debug(f"DISCONNECTED, {v_ip},{id}")
     return "DISCONNECTED" , v_ip,id
 
-def connect(serverid=947373,disconnect_current = True):
+def connect(serverid=947373):
     win_cmd = f'nordvpn -c -i {serverid}'
     subprocess.Popen(win_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
     #Wait till connected
     for i in range(10):
-        state,_,_ = status()
+        try:
+            state,_,_ = status()
+        except TypeError as error:
+            logging.debug("Could Not Connect , re-tries exceeded {}".format(error))
+            return ("Could Not Connect , re-tries exceeded {}".format(error))
+
         if state == 'DISCONNECTED':
-            print(f'Waiting for connection {i}')
+            logging.debug(f'Trying Connection : {i}...')
             sleep(3)
             state,_,_ = status()
         else:
-            return status()
-    return status()
+            retstatus = status()
+            logging.debug(retstatus)
+            return retstatus
+    retstatus = status()
+    logging.debug(retstatus)
+    return retstatus
 
 
 
 def disconnect():
     subprocess.Popen('nordvpn -d',stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-    return status()
+    retstatus = status()
+    logging.debug(retstatus)
+    return retstatus
     
 if __name__ == "__main__":
     print(disconnect())
