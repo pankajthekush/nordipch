5729e4c4edf1223d0999aba771bf965d27d2cb12
pankajthekush <pankajthekush@gmail.com>
2020-01-09 11:18:22 +0530

    Updated to V.05

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nordipch",
-    version="0.0.4",
+    version="0.0.5",
     author="Pankaj Kumar",
     author_email="pankajthekush@gmail.com",
     description="A Package to change NordVPN servers",
