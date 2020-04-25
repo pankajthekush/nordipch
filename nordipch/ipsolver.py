ea9344993cc260152e338ed6a327504a0819d1d9
pankajthekush <pankaj.kushwaha@rho.ai>
2020-04-25 13:12:24 +0530

    update

--- nordipch/ipsolver.py
+++ nordipch/ipsolver.py
@@ -4,23 +4,29 @@ from nordipch import connect
 rs = None
 driver = None
 
-def connect_vpn(vpname):
+def connect_vpn(vpname,user_agent):
+    #rs.driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":"python 2.7", "platform":"Windows"})
+    
     _,_,_,status = connect(serverdomain=vpname)
 
     if status:
-        run_chrome()
+        run_chrome(user_agent)
     else:
         print('can not connect to vpn')
 
 
-def run_chrome():
+def run_chrome(user_agent):
     rs = RemoteSelenium()
     driver = rs.driver
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":user_agent})
     driver.get('https://www.google.com/search?q=hello+world')
-
+    input('hit enter to quit and work with next ip')
     
 
 import os
 if __name__ == "__main__":
-    vpnfile = os.path.join('ovpn_tcp','us4633.nordvpn.com.tcp.ovpn')
-    connect_vpn(vpname=vpnfile)
+
+    vpnserver = input('enter vpn servername :')
+    user_agent = input('enter user agent :')
+    vpnfile = os.path.join('ovpn_tcp',vpnserver)
+    connect_vpn(vpname=vpnfile,user_agent=user_agent)
