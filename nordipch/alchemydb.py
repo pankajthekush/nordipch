21b4c831b8ca254221c525f7176fd1abc027f46c
pankajthekush <pankaj.kushwaha@rho.ai>
2020-05-04 23:36:54 +0530

    Update alchemydb.py

--- nordipch/alchemydb.py
+++ nordipch/alchemydb.py
@@ -1,48 +1,9 @@
 
-class Employee():
-    def __init__(self,first,last,pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
+from sqlalchemy import MetaData
+from sqlalchemy import Column,String
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-
-
-
-import sqlite3
-conn = sqlite3.connect('employee.db')
-cur = conn.cursor()
-# cur.execute("""CREATE TABLE employees(
-#             first text,
-#             last text,
-#             sal integer
-#             )""")
-
-
-# cur.execute("""CREATE TABLE employees (
-#             first text,
-#             last text,
-#             pay integer
-#             )""")
-
-
-def insert(emp):
-    with conn:
-        cur.execute("INSERT INTO employees VALUES(?,?,?)",(emp.first,emp.last,emp.pay))
-
-
-e1 = Employee('joh','doe',100)
-e2 = Employee('jak','june',100)
-
-
-
-cur.execute("INSERT INTO employees VALUES(:first,:last,:pay)",{'first':e2.first,'last':e2.last,'pay':e2 .pay})
-conn.commit()
-
-cur.execute("SELECT * from employees")
-
-print(cur.fetchall())
-
-conn.commit()
-conn.close()
-
-
+#source engine, and source table
+source_engine = create_engine()
