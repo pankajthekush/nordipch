3855cc64dd13a661ca2d05bf6ae2b89d9d6cf07d
root <pankajthekush@gmail.com>
2020-12-28 19:51:45 +0530

    update

--- nordipch/docker-runner.py
+++ nordipch/docker-runner.py
@@ -2,9 +2,12 @@ from ipvanish import change_ip
 
 
 
-if __name__ == "__main__":
+def docker_runner():
     while True:
         try:
             change_ip()
         except Exception:
             pass
+
+if __name__ == "__main__":
+    docker_runner()
